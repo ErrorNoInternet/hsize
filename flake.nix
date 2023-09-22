8ebd5fddaf4a9a2e5dc82dcc1b70ef01a03ae362
ErrorNoInternet <errornointernet@envs.net>
2023-09-22 12:11:49 +0800

    Add NixOS flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+    description = "Convert file sizes in bytes to human-readable units";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        (flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = nixpkgs.legacyPackages.${system};
+            in rec
+            {
+                packages.hsize = pkgs.rustPlatform.buildRustPackage {
+                    pname = "hsize";
+                    version = "1.1.0";
+                    cargoLock.lockFile = ./Cargo.lock;
+                    src = pkgs.lib.cleanSource ./.;
+                };
+                defaultPackage = packages.hsize;
+            }
+        ));
+}
