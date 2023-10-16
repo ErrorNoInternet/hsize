9a279494719d5dd36d8267e14f5c642f182d4caf
ErrorNoInternet <errornointernet@envs.net>
2023-10-16 20:06:47 +0800

    4 -> 2 spaces

--- flake.nix
+++ flake.nix
@@ -1,24 +1,24 @@
 {
-    description = "Convert file sizes in bytes to human-readable units";
+  description = "Convert file sizes in bytes to human-readable units";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, flake-utils }:
-        (flake-utils.lib.eachDefaultSystem (system:
-            let
-                pkgs = nixpkgs.legacyPackages.${system};
-            in rec
-            {
-                packages.hsize = pkgs.rustPlatform.buildRustPackage {
-                    pname = "hsize";
-                    version = "1.1.0";
-                    cargoLock.lockFile = ./Cargo.lock;
-                    src = pkgs.lib.cleanSource ./.;
-                };
-                defaultPackage = packages.hsize;
-            }
-        ));
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec
+      {
+        packages.hsize = pkgs.rustPlatform.buildRustPackage {
+          pname = "hsize";
+          version = "1.1.0";
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+        };
+        defaultPackage = packages.hsize;
+      }
+    ));
 }
