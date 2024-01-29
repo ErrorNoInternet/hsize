a3a66200fa87cb30bc9ef35314140fbb948b300b
ErrorNoInternet <errornointernet@envs.net>
2024-01-29 22:10:03 +0800

    chore(flake): cleanups

--- flake.nix
+++ flake.nix
@@ -2,23 +2,27 @@
   description = "hsize - Convert file sizes from bytes to human-readable units";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec
-      {
-        packages.hsize = pkgs.rustPlatform.buildRustPackage {
-          pname = "hsize";
-          version = "1.1.0";
-          cargoLock.lockFile = ./Cargo.lock;
-          src = pkgs.lib.cleanSource ./.;
-        };
-        defaultPackage = packages.hsize;
-      }
-    ));
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: (flake-utils.lib.eachDefaultSystem (
+    system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec
+    {
+      packages.hsize = pkgs.rustPlatform.buildRustPackage {
+        pname = "hsize";
+        version = "dev";
+
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+      };
+      defaultPackage = packages.hsize;
+    }
+  ));
 }
