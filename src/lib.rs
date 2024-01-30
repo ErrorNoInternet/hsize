3b2122171cc18525ecee871ed6ac1977893efb4e
ErrorNoInternet <errornointernet@envs.net>
2024-01-30 12:02:42 +0800

    refactor: Converter interface

--- src/lib.rs
+++ src/lib.rs
@@ -14,41 +14,17 @@ pub enum SizeUnit {
     Y,
 }
 
-pub fn display_unit(binary: bool, unit: SizeUnit) -> String {
-    let mut output = format!("{unit:?}");
-    if unit != SizeUnit::B {
-        output.push('B');
-        if binary {
-            output.insert(1, 'i');
-        }
-    }
-    output
+pub struct Converter {
+    pub binary: bool,
+    pub precision: usize,
+    pub from: Option<SizeUnit>,
+    pub to: Option<SizeUnit>,
 }
 
-pub fn display_size(size: u128, binary: bool, unit: Option<SizeUnit>, precision: usize) -> String {
-    let divisor: u128 = if binary { 1024 } else { 1000 };
-    let mut current_size = size as f64;
-    let mut current_unit = SizeUnit::B;
-
-    if let Some(unit) = unit {
-        current_unit = unit;
-        current_size /= divisor.pow(current_unit as u32) as f64;
-    } else {
-        while current_size >= divisor as f64 {
-            if let Some(new_unit) = num_traits::FromPrimitive::from_u32(current_unit as u32 + 1) {
-                current_unit = new_unit;
-            } else {
-                break;
-            }
-            current_size /= divisor as f64;
-        }
+impl Converter {
+    pub fn humanize(&self, size: u128) -> String {
+        self.convert(size, SizeUnit::B)
     }
-    format!(
-        "{:.precision$} {}",
-        current_size,
-        display_unit(binary, current_unit)
-    )
-}
 
     pub fn convert(&self, size: u128, unit: SizeUnit) -> String {
         let divisor: u128 = if self.binary { 1024 } else { 1000 };
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,28 @@
+mod arguments;
+
+use arguments::Arguments;
 use clap::Parser;
-use hsize::{display_size, SizeUnit};
-
-/// Convert file sizes from bytes to human-readable units
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Arguments {
-    /// Display sizes in binary (powers of 1024)
-    #[arg(short, long)]
-    binary: bool,
-
-    /// The amount of decimal places to display
-    #[arg(short, long, default_value_t = 2)]
-    precision: usize,
-
-    /// The unit to display sizes in
-    #[arg(short, long)]
-    unit: Option<SizeUnit>,
-
-    /// The file sizes in bytes, to be converted to their appropriate units
-    #[arg(num_args = 1..)]
-    sizes: Vec<u128>,
-}
+use hsize::Converter;
 
 fn main() {
     let arguments = Arguments::parse();
-    for size in arguments.sizes {
-        println!(
-            "{}",
-            display_size(size, arguments.binary, arguments.unit, arguments.precision)
-        )
-    }
+    let converter = Converter {
+        binary: arguments.binary,
+        precision: arguments.precision,
+        from: arguments.from,
+        to: arguments.to,
+    };
 
+    for size in arguments.sizes {
+        println!("{}", converter.humanize(size))
+    }
     if !atty::is(atty::Stream::Stdin) {
         for line in std::io::stdin().lines().flatten() {
-            match line.trim().parse::<u128>() {
-                Ok(number) => println!(
-                    "{}",
-                    display_size(
-                        number,
-                        arguments.binary,
-                        arguments.unit,
-                        arguments.precision
-                    )
-                ),
-                Err(_) => eprintln!("invalid digit found in \"{line}\""),
+            if let Ok(number) = line.trim().parse::<u128>() {
+                println!("{}", converter.humanize(number));
+            } else {
+                eprintln!("invalid digit found in \"{line}\"");
             };
         }
-    }
+    };
 }
