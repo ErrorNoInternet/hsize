fb142c241a8bd35538451cefd3675c094d296d34
ErrorNoInternet <errornointernet@envs.net>
2024-02-01 13:54:51 +0800

    feat(cli): better argument parsing + environment variables

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,4 +1,4 @@
-use clap::{ArgAction, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 use hsize::Scale;
 
 /// Convert file sizes to and from human-readable units
@@ -6,7 +6,7 @@ use hsize::Scale;
 #[command(version)]
 pub struct Arguments {
     /// Number of decimal places to include in the converted number
-    #[arg(short, long, default_value_t = 2)]
+    #[arg(short, long, env = "HSIZE_PRECISION", default_value_t = 2)]
     pub precision: usize,
 
     /// Size scale of the specified (input) numbers
@@ -14,7 +14,7 @@ pub struct Arguments {
     pub from_scale: Option<Scale>,
 
     /// Whether or not the specified (input) numbers are powers of 2 (1K = 1024)
-    #[arg(short = 'B', long)]
+    #[arg(short = 'B', long, env = "HSIZE_FROM_BINARY")]
     pub from_binary: bool,
 
     /// Size scale of the converted numbers
@@ -22,14 +22,14 @@ pub struct Arguments {
     pub to_scale: Option<Scale>,
 
     /// Whether or not the converted numbers should be powers of 2 (1K = 1024)
-    #[arg(short = 'b', long)]
+    #[arg(short = 'b', long, env = "HSIZE_TO_BINARY")]
     pub to_binary: bool,
 
     /// Character(s) to put between the number and unit
-    #[arg(short, long, default_value = " ")]
+    #[arg(short, long, env = "HSIZE_SEPARATOR", default_value = " ")]
     pub separator: String,
 
-    #[arg(action = ArgAction::Append)]
+    #[arg(num_args = 1..)]
     pub sizes: Vec<u128>,
 
     #[command(subcommand)]
@@ -39,6 +39,7 @@ pub struct Arguments {
 #[derive(Debug, Subcommand)]
 pub enum MainSubcommand {
     /// Use regex to search and replace numbers from stdin
+    #[command(visible_aliases = ["r"])]
     Replace {
         /// Regex pattern to use for matching numbers
         #[arg(short = 'r', long, default_value = r"\d+")]
