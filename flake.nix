1765936723d074d4d41149f01693f65621eba040
ErrorNoInternet <errornointernet@envs.net>
2024-02-07 20:19:42 +0800

    chore: clean up systems

--- flake.nix
+++ flake.nix
@@ -15,10 +15,10 @@
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
         "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
       ];
       perSystem = {
         system,
