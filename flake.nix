88fafa7b25732697fcd7dcaeed9875f7ef4545fd
ErrorNoInternet <errornointernet@envs.net>
2024-05-06 19:18:24 +0800

    chore(nix): remove hardcoded targets

--- flake.nix
+++ flake.nix
@@ -30,10 +30,6 @@
         ...
       }: let
         rust = pkgs.rust-bin.nightly.latest.default.override {
-          targets = [
-            "x86_64-unknown-linux-gnu"
-            "x86_64-unknown-linux-musl"
-          ];
           extensions = [
             "rust-src"
             "rust-analyzer-preview"
